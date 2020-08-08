2719862a08237ca3cc05561425f39b2732f4a036
Chris Patuzzo <chris@patuzzo.co.uk>
2020-08-08 16:01:12 +0100

    Calculate anagrams of a string

--- ruby/lib/anagrams_of.rb
+++ ruby/lib/anagrams_of.rb
@@ -1,3 +1,11 @@
 def anagrams_of(string)
-  # TODO
+  if string.empty?
+    [string]
+  else
+    anagrams_of(string[1..]).flat_map do |anagram|
+      (0..anagram.length).map do |i|
+        anagram.clone.insert(i, string[0])
+      end
+    end
+  end
 end
