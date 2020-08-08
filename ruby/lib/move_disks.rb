213d8a2b57db40ebacd980a16b6c17e58fb1e6a8
Chris Patuzzo <chris@patuzzo.co.uk>
2020-08-08 16:09:33 +0100

    Solve Tower of Hanoi

--- ruby/lib/move_disks.rb
+++ ruby/lib/move_disks.rb
@@ -1,3 +1,13 @@
 def move_disks(tower, count, from, to)
-  # TODO
+  if count == 1
+    tower.move(from, to)
+  else
+    other = (%i[a b c] - [from, to]).first
+
+    tower = move_disks(tower, count - 1, from, other)
+    tower = tower.move(from, to)
+    tower = move_disks(tower, count - 1, other, to)
+
+    tower
+  end
 end
